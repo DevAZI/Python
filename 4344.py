4044fcd51591cc53375fc2e259a5244153e0d53f
DevAZI <80841284+DevAZI@users.noreply.github.com>
2023-02-08 23:15:16 +0900

    0208-1

--- 4344.py
+++ 4344.py
@@ -2,10 +2,11 @@ import math
 a = int(input())
 for i in range(a):
     arr = list(map(int, input().split()))
-    j = 1
-    totalSum = 0
-    for j in range(arr[0]):
-        totalSum += arr[j]
-    print(round(totalSum/arr[0], 3))    
-
-
+    
+    avg = sum(arr[1:])/arr[0]
+    cnt = 0
+    for i in arr[1:]:
+        if i > avg:
+            cnt  += 1
+    a = round(cnt/arr[0]*100,3)
+    print('%.3f' % a + '%')
