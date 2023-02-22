7c2b96ccc83a353bfff1e107a279382ecf056f45
DevAZI <80841284+DevAZI@users.noreply.github.com>
2023-02-22 13:20:43 +0000

    0221

--- Sort/11650.py
+++ Sort/11650.py
@@ -1,7 +1,12 @@
-# import sys
-# input = sys.stdin.readline
-a = input()
-
-for i in range(0, len(a)):
-#     arr[i][0],  arr[i][1] = map(int, input().split)
+import sys
+input = sys.stdin.readline
+a = int(input())
+arr = []
+input1=0; input2 = 0
+for _ in range(a):
+    input1, input2 = map(int,input().split()) 
+    arr.append((input2,input1))
+arr.sort()
+for i in range(a):
+     print(f"{arr[i][1]} {arr[i][0]}")
 
